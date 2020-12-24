21754adbf20e68141c6e8956a85e8e34851e2dca
Apposmodding <souldbminer@192.168.0.151>
2020-12-24 16:25:17 +0530

    new stuff

---
+++ src/main/java/org/souldbminer/reallyinspace/keybind/OxygenStatsKeyBinding.java
@@ -0,0 +1,67 @@
+
+package org.souldbminer.reallyinspace.keybind;
+
+import org.souldbminer.reallyinspace.ReallyinspaceModElements;
+
+import org.lwjgl.glfw.GLFW;
+
+import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.client.settings.KeyBinding;
+
+import java.util.function.Supplier;
+
+@ReallyinspaceModElements.ModElement.Tag
+public class OxygenStatsKeyBinding extends ReallyinspaceModElements.ModElement {
+	@OnlyIn(Dist.CLIENT)
+	private KeyBinding keys;
+	public OxygenStatsKeyBinding(ReallyinspaceModElements instance) {
+		super(instance, 262);
+		elements.addNetworkMessage(KeyBindingPressedMessage.class, KeyBindingPressedMessage::buffer, KeyBindingPressedMessage::new,
+				KeyBindingPressedMessage::handler);
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void initElements() {
+		keys = new KeyBinding("key.mcreator.oxygen_stats", GLFW.GLFW_KEY_Y, "key.categories.reallyinspace");
+		ClientRegistry.registerKeyBinding(keys);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@SubscribeEvent
+	@OnlyIn(Dist.CLIENT)
+	public void onKeyInput(InputEvent.KeyInputEvent event) {
+	}
+	public static class KeyBindingPressedMessage {
+		int type, pressedms;
+		public KeyBindingPressedMessage(int type, int pressedms) {
+			this.type = type;
+			this.pressedms = pressedms;
+		}
+
+		public KeyBindingPressedMessage(PacketBuffer buffer) {
+			this.type = buffer.readInt();
+			this.pressedms = buffer.readInt();
+		}
+
+		public static void buffer(KeyBindingPressedMessage message, PacketBuffer buffer) {
+			buffer.writeInt(message.type);
+			buffer.writeInt(message.pressedms);
+		}
+
+		public static void handler(KeyBindingPressedMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
+			NetworkEvent.Context context = contextSupplier.get();
+			context.enqueueWork(() -> {
+			});
+			context.setPacketHandled(true);
+		}
+	}
+}
